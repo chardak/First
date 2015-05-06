3493db24b68de4a73ef678e65cd6d2a9fba45e47
chardak <chardakliev@gmail.com>
2015-05-06 22:37:08 +0300

    Domashna

---
+++ HelloCsharpHomework/ThisIsMyFirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsMyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plamen Vasilev Chardakliev");
+            Console.WriteLine();
+            Console.WriteLine("6 May 2015");
+            Console.ReadKey();
+        }
+    }
+}
